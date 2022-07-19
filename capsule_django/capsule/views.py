9a43b3caf223eeca6118d3aa046e9e605ffb6838
chamomiletay <chamomile.tay@gmail.com>
2022-07-19 14:10:12 -0400

    Defined 'show_articles' view

--- capsule_django/capsule/views.py
+++ capsule_django/capsule/views.py
@@ -1,3 +1,17 @@
+#---- i m p o r t s ! ----
 from django.shortcuts import render
+from django.http import JsonResponse, HttpResponse
+from rest_framework import generics, permissions
+from .models import Article
+from .serializers import ArticleSerializer
 
-# Create your views here.
+
+#---- v i e w s ! ----
+def working(request):
+    return HttpResponse('Hello, I am working!')
+
+#--- display wardrobe items ---
+def show_articles(request):
+    articles = Article.objects.all().values()
+    articles_list = list(articles)
+    return JsonResponse(articles_list, safe=False)
