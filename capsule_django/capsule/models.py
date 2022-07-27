e01b7f58f495782ab2b91af7070773d70dfe89e9
chamomiletay <chamomile.tay@gmail.com>
2022-07-27 12:42:10 -0400

    Updated Article model choices

--- capsule_django/capsule/models.py
+++ capsule_django/capsule/models.py
@@ -39,53 +39,52 @@ class Article(models.Model):
 
     #---- define clothing choices for user to select ----
     class ArticleType(models.TextChoices):
-        TOP = 'Top'
-        BTM = 'Bottoms'
-        DRESS = 'Dress'
-        JMPST = 'Jumpsuit/Romper'
-        SWTR = 'Sweater'
-        CRDGN = 'Cardigan'
-        OTRWR = 'Outerwear'
-        BDYST = 'Bodysuit'
-        LNGRE = 'Lingerie'
-        SHOES = 'Shoes'
-        ACCSRY = 'Accessory'
-        SOCKS = 'Socks/Tights'
+        TOP = 'Top', 'Top'
+        BTM = 'Bottoms', 'Bottoms'
+        DRESS = 'Dress', 'Dress'
+        JMPST = 'Jumpsuit/Romper', 'Jumpsuit/Romper'
+        SWTR = 'Sweater', 'Sweater'
+        CRDGN = 'Cardigan', 'Cardigan'
+        OTRWR = 'Outerwear', 'Outerwear'
+        BDYST = 'Bodysuit', 'Bodysuit'
+        LNGRE = 'Lingerie', 'Lingerie'
+        SHOES = 'Shoes', 'Shoes'
+        ACCSRY = 'Accessory', 'Accessory'
+        SOCKS = 'Socks/Tights', 'Socks/Tights'
 
     #---- define clothing categories ----
     class CategoryType(models.TextChoices):
-        LNGEWR = 'Loungewear'
-        CSUL = 'Casual'
-        FRML = 'Formal'
-        PRTY = 'Party'
-        WRK = 'Work'
+        LNGEWR = 'Loungewear', 'Loungewear'
+        CSUL = 'Casual', 'Casual'
+        FRML = 'Formal', 'Formal'
+        PRTY = 'Party', 'Party'
+        WRK = 'Work', 'Work'
 
 
     #---- define clothing color options ----
     class Color(models.TextChoices):
-        RED = 'Red'
-        ORNGE = 'Orange'
-        YLW = 'Yellow'
-        BLUE = 'Blue'
-        GRN = 'Green'
-        WHITE = 'White'
-        BGE = 'Beige'
-        CREAM = 'Cream'
-        BLK = 'Black'
-        PRPLE = 'Purple'
-        PNK = 'Pink'
-        GRAY = 'Gray'
-        BRWN = 'Brown'
-        TAN = 'Tan'
-        MLTI = 'Multi'
+        RED = 'Red', 'Red'
+        ORNGE = 'Orange', 'Orange'
+        YLW = 'Yellow', 'Yellow'
+        BLUE = 'Blue', 'Blue'
+        GRN = 'Green', 'Green'
+        WHITE = 'White', 'White'
+        BGE = 'Beige', 'Beige'
+        CREAM = 'Cream', 'Cream'
+        BLK = 'Black', 'Black'
+        PRPLE = 'Purple', 'Purple'
+        PNK = 'Pink', 'Pink'
+        GRAY = 'Gray', 'Gray'
+        BRWN = 'Brown', 'Brown'
+        TAN = 'Tan', 'Tan'
+        MLTI = 'Multi', 'Multi'
 
-    # uuid = models.UUIDField(unique=True, auto_created=True, default=uuid.uuid4)
     user = models.CharField(max_length=255, default='')
     name = models.CharField(max_length=255)
-    color = models.CharField(max_length=15, choices=Color.choices, default=Color.RED)
+    color = models.CharField(max_length=15, choices=Color.choices, default='Color.RED')
     # image = models.ImageField(upload_to='images/', default='')
-    article_type = models.CharField(max_length=15, choices=ArticleType.choices, default=ArticleType.TOP)
-    category = models.CharField(max_length=15, choices=CategoryType.choices, default=CategoryType.LNGEWR)
+    article_type = models.CharField(max_length=15, choices=ArticleType.choices, default='ArticleType.TOP')
+    category = models.CharField(max_length=15, choices=CategoryType.choices, default='CategoryType.WRK')
     brand = models.CharField(max_length=255)
     quantity = models.IntegerField()
 
